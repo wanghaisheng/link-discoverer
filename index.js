d7e303e352b73aae982593ecf039d63167d86214
David Miller <david.miller@getg5.com>
2020-10-15 14:10:59 -0700

    revised for cloud run

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const LinkDiscoverer = require('./linkDiscoverer')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(bodyParser.json({ limit: '1000kb' }))
+
 async function rover(url) {
   const linkDiscoverer = new LinkDiscoverer(url)
   await linkDiscoverer.run()
   return linkDiscoverer.sitemap
 }
-exports.rover = async (req, res) => {
+
+app.get('/', (req, res) => {
+  res.send('I\'m Here!')
+})
+
+app.post('/discover', async (req, res) => {
   const { url } = req.body
   const sitemap = await rover(url)
   res.json(sitemap)
-}
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
